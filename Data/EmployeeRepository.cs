56675af5444017bb2fcd37045d0dff7df365cb0e
Thais Guide <thaistguide@gmail.com>
2024-09-20 17:02:45 +0100

    Add project files.

---
+++ Data/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+namespace EmpolyeeM.Data
+{
+    public class EmployeeRepository
+    {
+        private  SqlConnection _connection;
+        public EmployeeRepository()
+        {
+            string connStr = "server=LAPTOP-NTBOS8PM\\SQLEXPRESS;database=EmployeeM;" +
+            "Integrated Security = true; TrustServerCertificate = True;";
+
+            _connection = new SqlConnection(connStr);
+         }
+
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmpolyeeM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
