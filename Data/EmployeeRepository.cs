31faae227445e5be735edd231c27b34eb6a516e7
Thais Guide <thaistguide@gmail.com>
2024-09-23 16:21:01 +0100

    StartSetUp

--- Data/EmployeeRepository.cs
+++ Data/EmployeeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.SqlClient;
+﻿using EmpolyeeM.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
 namespace EmpolyeeM.Data
 {
     public class EmployeeRepository
@@ -12,6 +15,36 @@ namespace EmpolyeeM.Data
             _connection = new SqlConnection(connStr);
          }
 
-        
+        public List<EmployeeEntity> GetAllEmployeeM ()
+        {
+            List<EmployeeEntity> EmployeeListEntity=new List<EmployeeEntity>();
+
+            SqlCommand cmd = new SqlCommand("GetAllEmployeeM", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            
+            dataAdapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeListEntity.Add(
+                    new EmployeeEntity
+                    {
+                        Id = Convert.ToInt32(dr["Id"]),
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        Email = dr["Email"].ToString(),
+                        Mobile = dr["Mobile"].ToString(),
+                        DOB = dr["DOB"].ToString(),
+                    });
+            }
+
+            return EmployeeListEntity;
+        }
+
+    
     }
-}
+} 
