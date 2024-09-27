44a38f58b97acdb89323c9ee70bff22c87bc45bb
Thais Guide <thaistguide@gmail.com>
2024-09-27 19:35:26 +0100

    Add Edit page functionality (Employee data not populating yet)

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -18,6 +18,41 @@ namespace EmpolyeeM.Controllers
         }
 
         [HttpGet]
+        public IActionResult EditEmployee (int Id)
+        {
+            EmployeeEntity Employee = new EmployeeEntity();
+
+            EmployeeRepository EmployeeRepository = new EmployeeRepository();
+
+            Employee = EmployeeRepository.GetEmployeeById(Id);
+
+            return View("EditEmployee", Employee);
+        }
+
+
+        [HttpPost]
+        public IActionResult EditEmployeeDetails(int Id, EmployeeEntity EmployeeDetails)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmployeeRepository _DbEmployee = new EmployeeRepository();
+                    if (_DbEmployee.EditEmployeeDetails(Id, EmployeeDetails))
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                return View(EmployeeDetails);
+            }
+            catch
+            {
+                return View(EmployeeDetails);
+            }
+        }
+
+        [HttpGet]
         public ActionResult AddEmployee()
         {
             return View();  // Returns the AddEmployee form view
--- Data/EmployeeRepository.cs
+++ Data/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeM.Models;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Reflection;
 namespace EmployeeM.Data
@@ -7,6 +8,9 @@ namespace EmployeeM.Data
     public class EmployeeRepository
     {
         private SqlConnection _connection;
+
+        public EmployeeEntity EmployeeEntity { get; private set; }
+
         public EmployeeRepository()
         {
             string connStr = "server=LAPTOP-NTBOS8PM\\SQLEXPRESS;database=EmployeeM;" +
@@ -51,6 +55,38 @@ namespace EmployeeM.Data
         }
 
 
+        public EmployeeEntity GetEmployeeById(int Id)
+        {
+            EmployeeEntity employeeEntity = null;  // Initialize as null to handle cases where no data is found
+
+            SqlCommand cmd = new SqlCommand("GetEmployeeDetailsById", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("@Id", Id));  // Use '@' for parameterized queries
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            dataAdapter.Fill(dt);
+
+            if (dt.Rows.Count > 0)  // Check if there are any rows
+            {
+                DataRow dr = dt.Rows[0];  // Since ID is unique, expect only one row
+                employeeEntity = new EmployeeEntity
+                {
+                    Id = Convert.ToInt32(dr["Id"]),
+                    FirstName = dr["FirstName"].ToString(),
+                    LastName = dr["LastName"].ToString(),
+                    Email = dr["Email"].ToString(),
+                    Mobile = dr["Mobile"].ToString(),
+                    DOB = dr["DOB"].ToString(),
+                };
+            }
+
+            return EmployeeEntity;  // Return the populated object or null if no data was found
+        }
+
+
         public bool AddEmployee(EmployeeEntity Employee)
         {
             SqlCommand cmd = new SqlCommand("AddEmployee", _connection);
@@ -68,13 +104,40 @@ namespace EmployeeM.Data
             int i = cmd.ExecuteNonQuery();
             _connection.Close();
 
-            if (i >= 1)
+            if (i > 0)
             {
                 return true;
             }
             else {
                 return false;
                 }
+        }
+
+        public bool EditEmployeeDetails(int Id, EmployeeEntity Employee)
+        {
+            SqlCommand cmd = new SqlCommand("EditEmployee", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Id", Id);
+
+            cmd.Parameters.AddWithValue("@FirstName", Employee.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", Employee.LastName);
+            cmd.Parameters.AddWithValue("@Email", Employee.Email);
+            cmd.Parameters.AddWithValue("@Mobile", Employee.Mobile);
+            cmd.Parameters.AddWithValue("@DOB", Employee.DOB);
+
+            _connection.Open();
+
+            int i = cmd.ExecuteNonQuery();
+            _connection.Close();
+
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
